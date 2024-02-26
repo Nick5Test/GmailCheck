158ca2cbc3c49bfe21422a9e4a2acf3965e1b917
Nick5Test <113012871+Nick5Test@users.noreply.github.com>
2024-02-26 14:45:43 +0100

    Verifica se ci sono email non lette in inbox. Verifica il check della frase in ogni email non letta dell'inbox.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,7 +14,8 @@ public class Main {
         final String username = email;
         final String host = "imap.gmail.com";
         String targetPhrase = "Attenzione! Non è stato possibile richiamare le informazioni sul luogo di accesso.";
-        boolean emailExists = false;
+        boolean phraseExists = false;
+        int unreadMessages = 0;
 
         Properties properties = new Properties();
         properties.put("mail.imap.host", host);
@@ -35,34 +36,56 @@ public class Main {
             folder.open(Folder.READ_WRITE);
             Message[] messages = folder.getMessages();
             Message lastMessage = messages[messages.length - 1];
-            System.out.println("messages.length---" + messages.length);
-            System.out.println("Subject: " + lastMessage.getSubject());
-            System.out.println("From: " + lastMessage.getFrom()[0]);
-            //latestMessage.setFlag(Flags.Flag.SEEN, true); //Segna il messaggio come leto
-            Object content = lastMessage.getContent();
-            if (content instanceof MimeMultipart) {
-                MimeMultipart multipart = (MimeMultipart) content;
-                for (int i = 0; i < multipart.getCount(); i++) {
-                    BodyPart bodyPart = multipart.getBodyPart(i);
-                    if (bodyPart.isMimeType("multipart/related")) {
-                        // Handle related multipart
-                        MimeMultipart relatedMultipart = (MimeMultipart) bodyPart.getContent();
-                        for (int j = 0; j < relatedMultipart.getCount(); j++) {
-                            BodyPart relatedBodyPart = relatedMultipart.getBodyPart(j);
-                            if (relatedBodyPart.isMimeType("text/html")) {
-                                // Print HTML content
-                                String htmlContent = (String) relatedBodyPart.getContent();
-                                //System.out.println("HTML Content:");
-                                //System.out.println(relatedBodyPart.getContent());
-                                if(htmlContent.contains(targetPhrase)){
-                                    System.out.println("Frase trovata");                                }
+            for (Message message : messages){
+                Flags flags = message.getFlags();
+                if(!flags.contains(Flags.Flag.SEEN)){
+                    unreadMessages ++;
+                    System.out.println("messages.length---" + messages.length);
+                    System.out.println("Subject: " + message.getSubject());
+                    System.out.println("From: " + message.getFrom()[0]);
+                    //latestMessage.setFlag(Flags.Flag.SEEN, true); //Segna il messaggio come leto
+                    Object content = message.getContent();
+                    if (content instanceof MimeMultipart) {
+                        MimeMultipart multipart = (MimeMultipart) content;
+                        for (int i = 0; i < multipart.getCount(); i++) {
+                            BodyPart bodyPart = multipart.getBodyPart(i);
+                            if (bodyPart.isMimeType("multipart/related")) {
+                                // Handle related multipart
+                                MimeMultipart relatedMultipart = (MimeMultipart) bodyPart.getContent();
+                                for (int j = 0; j < relatedMultipart.getCount(); j++) {
+                                    BodyPart relatedBodyPart = relatedMultipart.getBodyPart(j);
+                                    if (relatedBodyPart.isMimeType("text/html")) {
+                                        // Print HTML content
+                                        String htmlContent = (String) relatedBodyPart.getContent();
+                                        //System.out.println("HTML Content:");
+                                        //System.out.println(relatedBodyPart.getContent());
+                                        if(htmlContent.contains(targetPhrase)){
+                                            System.out.println("Frase trovata");
+                                            message.setFlag(Flags.Flag.SEEN, true);
+                                            phraseExists = true;
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
+                    else if (content instanceof String) {
+                        if(((String) content).contains(targetPhrase)){
+                            message.setFlag(Flags.Flag.SEEN, true);
+                            System.out.println("Phrase found !");
+                            phraseExists = true;
+                        }
+                    }
+                    if(phraseExists == false){
+                        System.out.println("Frase non esiste");
+                    }
                 }
             }
-            //System.out.println(result);
-            System.out.println("*********************************************");
+            if(unreadMessages == 0){
+                System.out.println("0 NEW email found in INBOX :( ");
+            }else{
+                System.out.println(unreadMessages+ " NEW email found unread in INBOX ");
+            }
 
             folder.close(false);
             store.close();
